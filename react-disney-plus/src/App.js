481309f3f1d1b7e384d9d405692568fb2135052f
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-08-14 23:16:32 +0900

    [Feat]video banner

--- react-disney-plus/src/App.js
+++ react-disney-plus/src/App.js
@@ -22,11 +22,19 @@ const Container = styled.main`
 	padding: 0 calc(3.5vw + 5px);
 
 	&:after {
+		/* background: url("/images/스크린샷 2023-07-08 오후 9.52.57.png") center center / cover no-repeat fixed;
+		background-size: contain; */
 		background: url("/images/home-background.png") center center / cover no-repeat fixed;
 		content: "";
 		position: absolute;
 		inset: 0px;
 		opacity: 1;
 		z-index: -1;
+	}
+	@media (max-width: 1280px) {
+		&:after {
+			background-size: 1280px 800px;
+			background-position: center top;
+		}
 	}
 `;
--- react-disney-plus/src/components/MovieModal/Banner.js
+++ react-disney-plus/src/components/MovieModal/Banner.js
@@ -1,9 +1,9 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import * as Api from "../../api";
 
 export const Banner = () => {
 	const [movie, setMovie] = useState([]);
+	const [isClicked, setClicked] = useState(false);
 
 	useEffect(() => {
 		fetchData();
@@ -20,8 +20,29 @@ export const Banner = () => {
 			params: { append_to_response: "videos" },
 		});
 		setMovie(movieDetail);
-		console.log(movie);
 	};
 
-	return <div>Banner</div>;
+	const truncate = (str, n) => {
+		return str?.length > n ? str.substring(0, n) + "..." : str;
+	};
+
+	return (
+		<header
+			className='banner'
+			style={{
+				backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+				backgroundPosition: "top center",
+				backgroundSize: "cover",
+				// height: 300,
+			}}>
+			<div className='banner__contents'>
+				<h1 className='banner__title'>{movie.title || movie.name || movie.original_name}</h1>
+				<div className='banner__buttons'>
+					{movie?.videos?.results[0]?.key && <button className='banner__button play'>Play</button>}
+				</div>
+				<p className='banner__description'>{truncate(movie.overview, 10)}</p>
+			</div>
+			<div className='banner--fadeBottom'></div>
+		</header>
+	);
 };
