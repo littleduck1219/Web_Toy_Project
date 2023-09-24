0941fc095f3a90e68186b6c28b5f5c49cfb56ff2
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-09-24 18:44:47 +0900

    [Feat]cart select all

--- shopping-mall/src/Components/cart/index.tsx
+++ shopping-mall/src/Components/cart/index.tsx
@@ -1,18 +1,36 @@
+import { SyntheticEvent, createRef, useRef } from "react";
 import { CartType } from "../../graphqlType";
-import { CartItem } from "./item";
 
 export const CartList = ({ items }: { items: CartType[] }) => {
+	const formRef = useRef<HTMLFormElement>(null);
+	const checkboxRefs = items.map(() => createRef<HTMLInputElement>());
+	const handleCheckboxChanged = (e: SyntheticEvent) => {
+		if (!formRef.current) return;
+		const targetInput = e.target as HTMLInputElement;
+		const data = new FormData(formRef.current);
+		const selectedCount = data.getAll("select-item").length;
+
+		if (targetInput.classList.contains("select-all")) {
+			const allChecked = targetInput.checked;
+			checkboxRefs.forEach((inputElem) => {
+				inputElem.current!.checked = allChecked;
+			});
+		} else {
+			const allChecked = selectedCount === items.length;
+			formRef.current.querySelector<HTMLInputElement>(".select-all")!.checked = allChecked;
+		}
+	};
 	return (
-		<>
+		<form ref={formRef} onChange={handleCheckboxChanged}>
 			<label>
-				<input type='checkbox' />
+				<input className='select-all' name='select-all' type='checkbox' />
 				전체선택
 			</label>
 			<ul className='cart'>
 				{items.map((item) => (
-					<CartItem {...item} key={item.id} />
+					<CartItem {...item} key={item.id} ref={checkboxRefs[i]} />
 				))}
 			</ul>
-		</>
+		</form>
 	);
 };
--- shopping-mall/src/Components/cart/item.tsx
+++ shopping-mall/src/Components/cart/item.tsx
@@ -2,9 +2,9 @@ import { useMutation } from "@tanstack/react-query";
 import { CartType } from "../../graphqlType";
 import { QueryKeys, getClient, graphqlFetcher } from "../../queryClient";
 import { DELETE_CART, UPDATE_CART } from "../../graphql/cart";
-import { SyntheticEvent } from "react";
+import { ForwardedRef, SyntheticEvent, forwardRef } from "react";
 
-export const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
+const CartItem = ({ id, imageUrl, price, title, amount }: CartType, ref: ForwardedRef<HTMLInputElement>) => {
 	const queryClient = getClient();
 	const { mutate: updateCart } = useMutation(
 		({ id, amount }: { id: string; amount: number }) => graphqlFetcher(UPDATE_CART, { id, amount }),
@@ -35,6 +35,7 @@ export const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
 
 	const handleUpdateAmount = (e: SyntheticEvent) => {
 		const amount = Number((e.target as HTMLInputElement).value);
+		if (amount < 1) return;
 		updateCart({ id, amount });
 		// updateCart(
 		// 	{ id, amount },
@@ -66,7 +67,7 @@ export const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
 	};
 	return (
 		<li className='cart-item'>
-			<input className='cart-item__checkbox' type='checkbox' />
+			<input className='cart-item__checkbox' type='checkbox' name={`select-item_${id}`} />
 			<img className='cart-item__image' src={imageUrl} />
 			<p className='cart-item__price'>{price}</p>
 			<p className='cart-item__title'>{title}</p>
@@ -77,3 +78,5 @@ export const CartItem = ({ id, imageUrl, price, title, amount }: CartType) => {
 		</li>
 	);
 };
+
+export default forwardRef(CartItem);
