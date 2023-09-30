df05ec2692560a331ad1128f16326ad45cb43b45
gyeong deok <107936957+littleduck1219@users.noreply.github.com>
2023-09-30 18:35:17 +0900

    [Feat]AuthContext
    로그인 사용자 정보 contextAPI로 관리

--- blog/src/components/Profile.tsx
+++ blog/src/components/Profile.tsx
@@ -1,25 +1,30 @@
+import AuthContext from "context/AuthContext";
 import { getAuth, signOut } from "firebase/auth";
 import { app } from "firebaseApp";
+import { useContext } from "react";
 import { toast } from "react-toastify";
 
+const onSignOut = async () => {
+	try {
+		const auth = getAuth(app);
+		await signOut(auth);
+		toast.success("로그아웃 되었습니다.");
+	} catch (error: any) {
+		console.log(error);
+		toast.error("로그아웃에 실패하였습니다.");
+	}
+};
+
 const Profile = () => {
-	const onSignOut = async () => {
-		try {
-			const auth = getAuth(app);
-			await signOut(auth);
-			toast.success("로그아웃 되었습니다.");
-		} catch (error: any) {
-			console.log(error);
-			toast.error("로그아웃에 실패하였습니다.");
-		}
-	};
+	const { user } = useContext(AuthContext);
+
 	return (
 		<div className='profile__box'>
 			<div className='flex__box-lg'>
 				<div className='profile__image' />
 				<div>
-					<div className='profile__email'>test@test.com</div>
-					<div className='profile__name'>김유저</div>
+					<div className='profile__email'>{user?.email}</div>
+					<div className='profile__name'>{user?.displayName}</div>
 				</div>
 				<div role='presentation' className='profile__logout' onClick={onSignOut}>
 					로그아웃
--- blog/src/index.tsx
+++ blog/src/index.tsx
@@ -4,16 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import firebase from "firebaseApp";
+import { AuthContextProvider } from "context/AuthContext";
 
 console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-	<React.StrictMode>
+	<AuthContextProvider>
 		<Router>
 			<App />
 		</Router>
-	</React.StrictMode>
+	</AuthContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
